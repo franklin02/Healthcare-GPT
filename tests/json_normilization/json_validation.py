12281d7b28573c1d9ac3b3af90e7c60b882bd00e
Dolan Lavoie <dolanlavoie@u.boisestate.edu>
2026-05-28 10:50:58 -0600

    Fix imports

--- tests/json_normilization/json_validation.py
+++ tests/json_normilization/json_validation.py
@@ -19,7 +19,7 @@ from functools import lru_cache
 from pathlib import Path
 from typing import Any
 
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
